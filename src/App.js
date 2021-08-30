38c957620fe915f6ea9b3ba4b42c69c175af6452
“[firstname <--replace>
2021-08-30 10:34:08 +0200

    StateCheckpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+class App extends Component {
+  Person = {
+    show: false,
+    fullName: "Cherif Ayachi",
+    bio: "Hello world ! ",
+    proffesion: "Developer",
+    imageSrc:
+      "https://maroc-diplomatique.net/wp-content/uploads/2021/03/Q-e1614779499923.jpg",
+    test: true,
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  toggleShow = () => {
+    this.setState({ show: !this.Person.show });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Button variant="secondary" onClick={this.toggleShow}>
+          {this.Person.show ? "Hide" : "Show"}
+        </Button>
+        {!this.Person.show && this.Person.test}
+        <h3>{this.Person.fullName}</h3>
+        <h3>{this.Person.bio}</h3>
+        <h3>{this.Person.proffesion}</h3>
+        <img src={this.Person.imageSrc} alt="imagePerson" />
+      </div>
+    );
+  }
 }
 
 export default App;
